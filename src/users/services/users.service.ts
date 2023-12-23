910e198b9413433a3d5b3cb0667db620f9a8d24c
Olga Chernega <eliang.cheng@gmail.com>
2023-12-23 18:08:37 +0200

    feat: add pgClient and users integration

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,28 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
 import { v4 } from 'uuid';
-
 import { User } from '../models';
+import pgClient from '../../db';
 
 @Injectable()
 export class UsersService {
   private readonly users: Record<string, User>;
 
-  constructor() {
-    this.users = {}
+  async findOne(userName: string): Promise<User> {
+    const user = await pgClient('users').where('name', userName).first();
+    return user;
   }
 
-  findOne(userId: string): User {
-    return this.users[ userId ];
-  }
-
-  createOne({ name, password }: User): User {
+  async createOne({ name, password }: User): Promise<User> {
     const id = v4();
-    const newUser = { id: name || id, name, password };
-
-    this.users[ id ] = newUser;
+    const newUser = (await pgClient('users')
+      .insert({ name, password })
+      .returning('*')) as any as User;
 
     return newUser;
   }
-
 }
