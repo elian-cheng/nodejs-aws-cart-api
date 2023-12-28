9b13929f495fecc4069172220f1b2b2928bf3ce9
Olga Chernega <eliang.cheng@gmail.com>
2023-12-28 13:09:36 +0200

    feat: add Docker configs

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-
 import helmet from 'helmet';
-
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['log', 'error', 'warn'],
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   app.use(helmet());
+
   await app.listen(port);
 }
 bootstrap().then(() => {
